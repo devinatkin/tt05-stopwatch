12b1cc8ba517e57126f76bd9e91ceb6ec0039fe4
Devin <devinatkin@gmail.com>
2023-10-15 12:39:08 -0600

    fixing double dabble to run and test properly

--- src/double_dabble.sv
+++ src/double_dabble.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/12/2022 02:58:00 PM
-// Design Name: 
-// Module Name: doubleDabble
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// Based on the algorithmic description on Wikipedia https://en.wikipedia.org/wiki/Double_dabble
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module doubleDabble(
     input logic [13:0] bin,
@@ -26,58 +6,57 @@ module doubleDabble(
     input logic clk,
     input logic rst,
     output logic ready
-    );
-    
-    // Reserve a size that is big enough for both the BCD and binary representations (This scratch space)
+);
+
     logic [29:0] scratch;
     logic [3:0] clkcnt;
-    // Partition the space as so for our case
-    // 1000s 100s Tens Ones OriginalBinary
-    // 0000 0000 0000 0000 00000000000000
-    
-    //Iterate the algorithm for the number of bits in the input (14 times)
-    //On each iteration shift 1 left and if any BCD number is >=5 increment the value by 3
-    always_ff @(posedge clk)
-    begin
+    logic shift;
+    // Initialize scratch and clkcnt
+    always_ff @(posedge clk) begin
         if(rst) begin
-            scratch <= 0;
-            bcd <= 0;
-            ready <= 1;
-            clkcnt <= 0;
-        end else if(clk) begin
-            //Check If Ready
-            if (clkcnt == 0) begin
-                ready <= 1; //Set a Ready Flag
-                bcd[15:0] <= scratch[29:14]; //Set the BCD Output
-                scratch[29:14] <= 0;
-                scratch[13:0] <= bin;
-                clkcnt <= clkcnt + 1;
-            end else begin
-                scratch[29:0] <= {scratch[28:0], 1'b0}; //Shift Left
-                clkcnt <= clkcnt + 1;
-                ready <= 0; //Not Ready
-                
-            end
-            
-        end
-    end
-
-    always_ff @( negedge clk) begin
-        if(clkcnt == 15) begin
-            clkcnt <= 0;
+            scratch <= 30'h0;
+            clkcnt <= 4'h0;
+            ready <= 1'b0;
+            bcd <= 16'h0;
+            shift <= 1'b0;
         end else begin
-            if(scratch[29:26] >= 5) begin //Add to any bcd which is >= 5
-                    scratch[29:26] <= scratch[29:26] + 3;
+            // Shift when 0 add when 1
+            shift <= !shift;
+            if(clkcnt == 4'h0) begin
+                scratch[29:14] <= 16'h0;
+                scratch[13:0] <= bin;
+
+                clkcnt <= 4'h2;
+                bcd <= scratch[29:14];
+                ready <= 1'b1;
+            end else begin
+                ready <= 1'b0;
+                if (shift == 1'b1) begin // If Shift is 1 then add 3 to each digit if >= 5
+                    if (scratch[29:26] >= 5) begin 
+                        scratch[29:26] <= scratch[29:26] + 3;
+                    end
+                    if (scratch[25:22] >= 5) begin
+                        scratch[25:22] <= scratch[25:22] + 3;
+                    end
+                    if (scratch[21:18] >= 5) begin
+                        scratch[21:18] <= scratch[21:18] + 3;
+
+                    end
+                    if (scratch[17:14] >= 5) begin 
+                        scratch[17:14] <= scratch[17:14] + 3;
+                    end
+                end else begin // If Shift is 0 then shift left by 1
+
+                    // Shift left by 1 and fill with 0
+                    scratch[29:0] <= {scratch[28:0], 1'b0};
+
+                    // Increment clkcnt (Tracking the number of left shifts)
+                    clkcnt <= clkcnt + 1;
+                end
             end
-            if(scratch[25:22] >= 5) begin
-                    scratch[25:22] <= scratch[25:22] + 3;
-            end
-            if(scratch[21:18] >= 5) begin
-                    scratch[21:18]  <= scratch[21:18] + 3;
-            end
-            if(scratch[17:14] >= 5) begin
-                    scratch[17:14]  <= scratch[17:14] + 3;
-            end
+
+
+
         end
     end
 endmodule
